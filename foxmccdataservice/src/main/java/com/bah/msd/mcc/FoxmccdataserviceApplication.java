605a03f088c68c0445cedcbc9a2db4471c770555
Saravanan Kuppusamy <saravanan_kuppusamy@yahoo.com>
2022-08-16 10:42:03 -0400

    updated

--- foxmccdataservice/src/main/java/com/bah/msd/mcc/FoxmccdataserviceApplication.java
+++ foxmccdataservice/src/main/java/com/bah/msd/mcc/FoxmccdataserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoxmccdataserviceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("main begins");
 		SpringApplication.run(FoxmccdataserviceApplication.class, args);
 	}
 
